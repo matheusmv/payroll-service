d0e32636dde5bc7aa3e8e51effd31f4cd2882cc5
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-09-17 00:43:14 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/github/matheusmv/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/github/matheusmv/hroauth/services/UserService.java
@@ -6,12 +6,15 @@ import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -29,6 +32,21 @@ public class UserService {
             logger.error("Email not found: " + email);
 
             return Optional.empty();
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            var user = userFeignClient.getByEmail(username).getBody();
+
+            logger.error("Email found: " + username);
+
+            return user;
+        } catch (FeignException exception) {
+            logger.error("Email not found: " + username);
+
+            throw new UsernameNotFoundException("Email found: " + username);
         }
     }
 }
